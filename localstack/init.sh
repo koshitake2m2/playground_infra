862f7eafd92c0a40171cd0c49646770e4489e4c8
koshitake2m2 <takeru.koshimizu@bizreach.co.jp>
2023-07-19 13:22:15 +0900

    fix

--- localstack/init.sh
+++ localstack/init.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-aws sqs create-queue --queue-name play-sample-bucket --endpoint-url http://localhost:4567
+aws sqs create-queue --queue-name play-sample-queue --endpoint-url http://localhost:4567
+
+# see: https://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html#description
+# you must use `us-east-1` region.
+aws s3api create-bucket --bucket play-sample-bucket --region us-east-1 --endpoint-url http://localhost:4567
