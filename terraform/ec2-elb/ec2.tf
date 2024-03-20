628b9ee9492a53e649c34affab51f4756e25439c
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-03-20 16:21:23 +0900

    fix vpc_security_group_ids

--- terraform/ec2-elb/ec2.tf
+++ terraform/ec2-elb/ec2.tf
@@ -1,11 +1,11 @@
 
 resource "aws_instance" "app_server_1a" {
-  ami             = "ami-0b828c1c5ac3f13ee"
-  key_name        = aws_key_pair.ssh_pub_key.key_name
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public_1a.id
-  security_groups = [aws_security_group.app_server_sg.id]
-  user_data       = file("./setup_1a.sh")
+  ami                    = "ami-0b828c1c5ac3f13ee"
+  key_name               = aws_key_pair.ssh_pub_key.key_name
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_1a.id
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  user_data              = file("./setup_1a.sh")
 
   tags = {
     Name = "app_server_1a"
@@ -13,12 +13,12 @@ resource "aws_instance" "app_server_1a" {
 }
 
 resource "aws_instance" "app_server_1c" {
-  ami             = "ami-0b828c1c5ac3f13ee"
-  key_name        = aws_key_pair.ssh_pub_key.key_name
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public_1c.id
-  security_groups = [aws_security_group.app_server_sg.id]
-  user_data       = file("./setup_1c.sh")
+  ami                    = "ami-0b828c1c5ac3f13ee"
+  key_name               = aws_key_pair.ssh_pub_key.key_name
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_1c.id
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  user_data              = file("./setup_1c.sh")
 
   tags = {
     Name = "app_server_1c"
